7a7e2bc6c45cf93ad635ca0e29dc547541ddfef4
emenjivar <carlos.itca@gmail.com>
2026-03-07 23:34:56 -0600

    feat: Request permissions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.emenjivar.simplebleclient"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.emenjivar.simplebleclient"
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.google.accompanist.permissions)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/emenjivar/simplebleclient/MainActivity.kt
+++ app/src/main/java/com/emenjivar/simplebleclient/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.emenjivar.simplebleclient
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,16 +12,48 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.emenjivar.simplebleclient.ui.theme.SimpleBLEClientTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SimpleBLEClientTheme {
+                val permission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    listOf(
+                        Manifest.permission.BLUETOOTH_SCAN,
+                        Manifest.permission.BLUETOOTH_CONNECT
+                    )
+                } else {
+                    listOf(Manifest.permission.ACCESS_FINE_LOCATION)
+                }
+                val permissionState = rememberMultiplePermissionsState(permission)
+
+                LaunchedEffect(permissionState) {
+                    when {
+                        permissionState.allPermissionsGranted -> {
+                            Log.wtf("MainActivity", "all permission granted")
+                        }
+
+                        permissionState.shouldShowRationale -> {
+                            Log.wtf("MainActivity", "Some denied but can still ask")
+                            permissionState.launchMultiplePermissionRequest()
+                        }
+
+                        // Never asked
+                        else -> {
+                            permissionState.launchMultiplePermissionRequest()
+                        }
+                    }
+                }
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
